b2bd8bbd779d7fd3525c40d6a721550f7479225a
asantesylvesterkwame <intelligentson17@gmail.com>
2023-08-26 01:48:51 +0000

    updated cross origin

--- apiNew/server.js
+++ apiNew/server.js
@@ -26,7 +26,7 @@ const connect = async () => {
   }
 };
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://job20.netlify.app/", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
