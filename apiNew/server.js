e5989a739d406e276b0074151d35f90c29534007
asantesylvesterkwame <intelligentson17@gmail.com>
2023-08-31 00:40:21 +0000

    cors

--- apiNew/server.js
+++ apiNew/server.js
@@ -27,6 +27,15 @@ const connect = async () => {
 };
 
 app.use(cors({ origin: "https://job-brown.vercel.app/", credentials: true }));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  next();
+});
 app.use(express.json());
 app.use(cookieParser());
 
